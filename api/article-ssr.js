91abf18b16237a0dc1edc534df6231632441201b
jfainef52 <jeff@enhancedfunding.com>
2026-04-27 15:00:28 -0400

    Update article-ssr.js

--- api/article-ssr.js
+++ api/article-ssr.js
@@ -1,4 +1,4 @@
-·—export default async function handler(req, res) {
+ ·—export default async function handler(req, res) {
   const { slug } = req.query;
   if (!slug) return res.status(400).send('Missing slug');
 
